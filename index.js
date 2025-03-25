bac8f96b6db6a19df482ba20c65581fd848c6714
Fabian A Ortiz Chavez <alonsofabianortiz@icloud.com>
2025-03-24 23:42:48 -0600

    method-lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+const drivers = ["Bobby", "Sammy", "Susan", "bobby"];
+ 
+function findMatching(drivers, nameToMatch) {
+    return drivers.filter(driver => 
+      driver.toLowerCase() === nameToMatch.toLowerCase()
+    );
+  }
+  function fuzzyMatch(drivers, letters){
+  return drivers.filter(driver =>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+    )
+  }
+  
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
+  
